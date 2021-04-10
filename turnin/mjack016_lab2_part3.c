f98620d05f5f8258aee5656da09a77e9a6127c18
Marod Jackson <mjack016@wch136-24.cs.ucr.edu>
2021-04-10 16:53:40 -0700

    fixed part 3

--- turnin/mjack016_lab2_part3.c
+++ turnin/mjack016_lab2_part3.c
@@ -17,25 +17,27 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xF0; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
-    /* Insert your solution below */
+    /* Insert your solution below */	
 	unsigned char tmpA = 0x00;
-	unsigned char cnt = 0;
-    while (1) {	
-	tmpA = PINA; 
-	if (PINA ==  0x00){
-        	cnt = 2;       
+        unsigned char cnt = 0;
+    while (1) {
+        tmpA = PINA;
+        if (tmpA ==  0x00 || tmpA == 0x80){
+                cnt = 4;
         }
-	else if (PINA == 0x01){
-		cnt = 1;
-	}
-	else if (PINA == 0x02){
-		cnt = 1;
+        else if (tmpA == 0x01 || tmpA == 0x02 || tmpA == 0x04 || tmpA == 0x08){
+                cnt = 3;
         }
-	else if (PINA == 0x03){
-		cnt = 0x80;
-		
+        else if (tmpA == 0x03 || tmpA == 5 || tmpA == 6 || tmpA == 0x0A || tmpA == 0x0C ){
+                cnt = 2;
         }
-	PORTC = cnt;
+        else if (tmpA == 0x03 || tmpA == 0x07 || tmpA == 0x0B || tmpA == 0x0D || tmpA == 0x0E){
+                cnt = 1;
+        }
+        else{
+                cnt = 0x80;
+        }
+        PORTC = cnt;
     }
     return 0;
 }
